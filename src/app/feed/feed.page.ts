ec7e8d2d8d17c5d5ba43e9a12ecda6e88ca1238f
Fran0liveira <=>
2020-08-16 13:57:01 -0300

    First commit

---
+++ src/app/feed/feed.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-feed',
+  templateUrl: './feed.page.html',
+  styleUrls: ['./feed.page.scss'],
+})
+export class FeedPage implements OnInit {
+
+  nomeUsuario: String = "Doc Brown";
+  postMessage: string = "Minha nova idea!!!";
+
+  constructor() {
+    
+   }
+
+  ngOnInit() {
+    this.somaDoisNumeros(10, 99);
+  }
+
+  somaDoisNumeros(num1: number, num2: number) : void {
+    //alert(num1 + num2);
+  }
+
+}
